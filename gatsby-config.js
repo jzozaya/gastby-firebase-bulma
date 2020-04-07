16dfa1a0bef4768276c54116ba65bba0f4610d55
Jorge Zozaya <jorge.zozaya.verdiguel@gmail.com>
2020-04-07 00:03:51 -0500

    New records and text edits

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,11 @@
 module.exports = {
 	siteMetadata: {
-		title: 'Gatsby Bulma Quickstart',
-		author: 'Aman Mittal',
+		title: 'Journey With Music',
+		author: 'Jorge Zozaya',
 		imageUrl: 'https://i.imgur.com/Vz81GEl.png',
-		description: 'A Project to bootstrap your next Gatsby + Bulma site.',
+		description: 'Amazing discoveries using Gatsby + Bulma site.',
 		keywords: `Web developer, Web, Developer, CSS, HTML, JS, Javascript, Gatsby, Bulma Developer, CSS3, HTML5, Seo, Starter`,
-		twitter: 'https://twitter.com/amanhimself',
-		github: `https://github.com/amandeepmittal`,
-		medium: 'https://medium.com/@amanhimself',
+		github: `https://github.com/jzozaya`,
 		gatsby: 'https://www.gatsbyjs.org/',
 		bulma: 'https://bulma.io/',
 		siteUrl: `https://www.example.com`
